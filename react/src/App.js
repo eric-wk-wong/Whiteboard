c7d334b7f5bb0172dc28905f7ce8f2aaaf9b637c
Eric <ericewonwong@gmail.com>
2020-02-05 19:35:20 -0500

    React Foundation

---
+++ react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Web App">
+      <header className="header">
+        <h1>Smurfs</h1>
+        <p>
+          This is a test paragraph.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
